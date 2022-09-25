6d04110e448889cf1affa0664892c09364ef5ecc
Anderson Silva <andssilva.23@gmail.com>
2022-09-25 00:50:11 -0400

    feat: :rocket: criando projeto e componente yes-no-button-group

---
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yes-no-button-group',
+  templateUrl: './yes-no-button-group.component.html',
+  styleUrls: ['./yes-no-button-group.component.scss']
+})
+export class YesNoButtonGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
