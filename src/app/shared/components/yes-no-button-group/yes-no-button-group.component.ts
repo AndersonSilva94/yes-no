b23799122e5e7db9f53be2629f4490bf54237c5e
Anderson Silva <andssilva.23@gmail.com>
2022-10-01 18:15:34 -0400

    feat: :rocket: criando a interface ControlValueAccessor

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,18 +1,35 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss']
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
 
   @Input() public value: string = null;
   @Input() public label!: string;
   @Output() public valueChange = new EventEmitter<string>();
   public options = YesNoButtonGroupOptions;
+  public onChange = (value: string) => {};
+  public onTouched = () => {};
 
   constructor() { }
+
+  public writeValue(value: string): void { //muda o valor da view
+    this.value = value
+    this.onChange(this.value);
+  }
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn
+  }
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
