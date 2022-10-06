b917e1b0363b230375d8528b8f1d4fceb17d0c51
Anderson Silva <andssilva.23@gmail.com>
2022-10-06 00:41:05 -0400

    feat: :rocket: criando uma diretiva para realizar o disable genérico

---
+++ src/app/shared/directives/disable-control/disable-control.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[appDisableControl]'
+})
+export class DisableControlDirective implements OnChanges {
+
+  @Input() appDisableControl = false;
+
+  constructor(
+    private ngControl: NgControl
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.appDisableControl) {
+      const action = this.appDisableControl ? 'disable' : 'enable';
+      this.ngControl.control[action]();
+    }
+  }
+
+}
